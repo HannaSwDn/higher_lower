264109176b7704c645ef9c4f3c0acd6f7a494cbf
hanna_nyman <hannanyman.sk@gmail.com>
2018-10-20 21:13:13 +0200

    Added a spin function to JavaScript file

--- front_end/script.js
+++ front_end/script.js
@@ -1,1 +1,11 @@
-console.log('empty');
+// variables
+let currentNumber;
+let lastNumber;
+let degrees = 2500;
+let wheel = document.getElementById('wheel');
+
+const spin = () => {
+    degrees = degrees + Math.floor(Math.random() * 360);
+    document.getElementById('wheel').style.transform = `rotate(${degrees}deg)`;
+    degrees = degrees + 2500;
+}
